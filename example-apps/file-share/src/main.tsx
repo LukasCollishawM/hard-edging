48c8b4c8c6fb5fb61a19af5a0b1290e6c92264e5
LukasCo <lukascollishaw@gmail.com>
2025-11-19 09:57:42 +0000

    removed example apps because 1. they dont exist 2. god forbid anyone actually uses this

--- example-apps/file-share/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-const App: React.FC = () => {
-  return <div>File Share Example (Coming Soon)</div>;
-};
-
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
-
